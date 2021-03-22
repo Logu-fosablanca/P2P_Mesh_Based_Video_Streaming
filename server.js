a9966ce4b8740602976bedb75ff6e5dca6c35055
logu-fosablanca <logu.r19@iiits.in>
2021-03-23 00:07:11 +0530

    Added Self View Video Feature

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
  const app = express();
  const server = require('http').Server(app)
  const { v4: uuidv4 } = require('uuid');
-
+app.use(express.static('public'))
  
 
 app.set('view engine','ejs');
