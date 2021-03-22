6dae4bf67612d74d50eb4f348f0b032c9d27f273
logu-fosablanca <logu.r19@iiits.in>
2021-03-23 03:43:53 +0530

    Reversed the Changes

--- server.js
+++ server.js
@@ -1,42 +1,45 @@
-const express = require('express')
-const app = express()
-// const cors = require('cors')
-// app.use(cors())
+const express = require('express');
+const app = express();
 const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { ExpressPeerServer } = require('peer');
-const peerServer = ExpressPeerServer(server, {
-  debug: true
+const { v4: uuidv4 } = require('uuid');
+
+const { uniqueNamesGenerator, adjectives, colors, animals } = require('unique-names-generator');
+const Username = uniqueNamesGenerator({
+    dictionaries: [adjectives, animals, colors], // colors can be omitted here as not used
+    length: 2
 });
-const { v4: uuidV4 } = require('uuid')
 
-app.use('/peerjs', peerServer);
+const io = require('socket.io')(server)
 
-app.set('view engine', 'ejs')
+const {ExpressPeerServer} = require('peer')
+const peerServer= ExpressPeerServer(server,{
+    debug: true,
+});
+
 app.use(express.static('public'))
 
+app.use('/peerjs',peerServer)
+
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
+    res.redirect(`/${uuidv4()}`);
 })
 
 app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+    res.render('room', { roomid: req.params.room ,userid:Username });
+
 })
 
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId);
-    // messages
-    socket.on('message', (message) => {
-      //send message to the same room
-      io.to(roomId).emit('createMessage', message)
-  }); 
+io.on('connection',(socket)=>{
+    socket.on('join-room',(roomid,USERID)=>{
+        socket.join(roomid)
+        socket.broadcast.to(roomid).emit('user-connected',USERID);
+        // console.log("Joined the Room");
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
-  })
 })
 
-server.listen(process.env.PORT||3030)
+
+
+server.listen(3030);
